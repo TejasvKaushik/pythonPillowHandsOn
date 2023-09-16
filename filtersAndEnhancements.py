2abfa8be189688d4f81f5bdc83816056c019c555
Tejasv Kaushik <tejasv2003@gmail.com>
2023-09-16 22:25:56 +0530

    multiple enhancements on the same image

--- filtersAndEnhancements.py
+++ filtersAndEnhancements.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageEnhance, ImageOps
 
 image = Image.open("images/earth.jpg")
 
@@ -14,8 +14,19 @@ enhancedImageContrast = contrastEnhancer.enhance(2)
 enhancedImageBrightness = brightnessEnhancer.enhance(2)
 enhancedImageSharpness = sharpnessEnhancer.enhance(5)
 
+# applying multiple enhancements
+enhancer = ImageEnhance.Sharpness(image)
+enhancedImage = enhancer.enhance(5)
+# enhancer = ImageEnhance.Brightness(enhancedImage)
+# enhancedImage = enhancer.enhance(2)
+enhancer = ImageEnhance.Contrast(enhancedImage)
+enhancedImage = enhancer.enhance(2)
+enhancer = ImageEnhance.Color(enhancedImage)
+enhancedImage = enhancer.enhance(0)
+
 #showing the image
 # enhancedImageColor.show()
 # enhancedImageContrast.show()
 # enhancedImageBrightness.show()
-enhancedImageSharpness.show()
+# enhancedImageSharpness.show()
+enhancedImage.show()
